9612cc2ee068ddc0bf5b50e127b7113d7a71d5be
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-06-25 10:08:23 +0530

    users table

--- myapp/src/Components/UsersData.js
+++ myapp/src/Components/UsersData.js
@@ -3,11 +3,22 @@ import React from 'react';
 export default function UserData(){
     return(
         <div className='container'>
+
             <h2>React Dynamic Table</h2>
+
             <div className='users'>
-            <h3>Username:</h3>
-            <input type='text' id='name'/>
+                <h3>Username:</h3>
+                <input type='text' id='name'/>
+                <h3>Email ID:</h3>
+                <input type='text' id='name'/>
+                <h3>Password:</h3>
+                <input type='password' id='name'/>
             </div>
+
+            <table>
+                <thead>Users Data</thead>
+            </table>
+
         </div>
     );
 }
