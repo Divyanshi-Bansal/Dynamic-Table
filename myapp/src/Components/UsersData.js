89c49cf12a7839a0cdb70c1feaaf1baa362bc2e4
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-06-25 10:05:33 +0530

    get users data

---
+++ myapp/src/Components/UsersData.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function UserData(){
+    return(
+        <div className='container'>
+            <h2>React Dynamic Table</h2>
+            <div className='users'>
+            <h3>Username:</h3>
+            <input type='text' id='name'/>
+            </div>
+        </div>
+    );
+}
